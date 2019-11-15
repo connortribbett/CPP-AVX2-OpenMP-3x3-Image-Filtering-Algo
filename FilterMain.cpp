70d41b51cb456814921963f8a26bbb2639a4ea51
connortribbett <49297850+connortribbett@users.noreply.github.com>
2019-11-15 16:15:45 -0700

    Commented

--- FilterMain.cpp
+++ FilterMain.cpp
@@ -122,15 +122,35 @@ applyFilter(struct Filter *filter, cs1300bmp *input, cs1300bmp *output)
     output->height = h + 1;
     
     __2in3Filter filt = filter->getFilter();
-
+    /*
+    Get two copies of the filter in 3 256bit vectrs(upper, middle, lower) in the format below
+    0 1 2 0 1 2 0 0
+    3 4 5 3 4 5 0 0
+    6 7 8 6 7 8 0 0
+    */
     __2in3Filter128 filt128;
     filt128.upper = _mm256_extractf128_ps(filt.upper, 0);
     filt128.middle = _mm256_extractf128_ps(filt.middle, 0);
     filt128.lower = _mm256_extractf128_ps(filt.lower, 0);
+    /*
+    Get one copy of the filter in 3 128bit vectors by extracting the lower half of the 256bit filter vector
+    0 1 2 0
+    3 4 5 3
+    6 7 8 6
+    Last value is always multiplied to 0 in 128bit calulations
+    */
     
     __m256i mask = _mm256_setr_epi32(-1, -1, -1, -1, -1, -1, 0, 0);
     __m128i mask128 = _mm_setr_epi32(-1, -1, -1, 0);
+    /*
+    Create a 256bit and 128bit mask, taking the data of 2 and 1 pixels respectively that are 3 pixels apart
+    Need 3 values per pixel, so the last 2 and 1 values are masked out respectively to not affect the calculation
+    */
     
+    /*
+    OpenMP runs each plane loop iteration in parallel as they are dependent
+    SIMD allows for further optimization due to the use of avx2 in the loop
+    */
     #pragma omp parallel for simd
     for(int plane = 0; plane < 3; plane++){
         for(int row = 1; row < h; row++) {
@@ -147,7 +167,9 @@ applyFilter(struct Filter *filter, cs1300bmp *input, cs1300bmp *output)
                 __m256 lower1 = _mm256_maskload_ps(&input->color[plane][row+1][col - 1], mask);
                 __m256 lower2 = _mm256_maskload_ps(&input->color[plane][row+1][col], mask);
                 __m256 lower3 = _mm256_maskload_ps(&input->color[plane][row+1][col + 1], mask);
-                
+                /*
+                Load 6 pixels of information into 9 vectors
+                */
                 
                 
                 middle1 = _mm256_mul_ps(middle1, filt.middle);
@@ -161,15 +183,21 @@ applyFilter(struct Filter *filter, cs1300bmp *input, cs1300bmp *output)
                 upper1 = _mm256_fmadd_ps(upper1, filt.upper, lower1);
                 upper2 = _mm256_fmadd_ps(upper2, filt.upper, lower2);
                 upper3 = _mm256_fmadd_ps(upper3, filt.upper, lower3);
+                /*
+                Multiplication and fused multiply + add are used to multiply each pixel by its respective
+                filter value and then added into upper
+                The sum of each pixel are now in the first 3 and next 3 values respectively
+                */
                 
-				//upper1 = _mm256_add_ps(upper1, _mm256_add_ps(lower1, middle1));
-                //upper2 = _mm256_add_ps(upper2, _mm256_add_ps(lower2, middle2));
-                //upper3 = _mm256_add_ps(upper3, _mm256_add_ps(lower3, middle3));
-                
+               
+                /*
+                Below the values of each pixel is added from their respective 3 vector elements and divided
+                The output pixel is then set after clamping the value to 255 with an inline function
+                */
                 float counter1 = (upper1[0] + upper1[1] + upper1[2])/divisor;
                 float counter2 = (upper2[0] + upper2[1] + upper2[2])/divisor;
                 float counter3 = (upper3[0] + upper3[1] + upper3[2])/divisor;
-                
+            
                 output->color[plane][row][col] = fClamp255(counter1);
                 output->color[plane][row][col + 1] = fClamp255(counter2);
                 output->color[plane][row][col + 2] = fClamp255(counter3);
@@ -186,12 +214,18 @@ applyFilter(struct Filter *filter, cs1300bmp *input, cs1300bmp *output)
                 __m128 upper = _mm_maskload_ps(&(input->color[plane][row - 1][col - 1]), mask128);
                 __m128 middle = _mm_maskload_ps(&(input->color[plane][row][col - 1]), mask128);
                 __m128 lower = _mm_maskload_ps(&(input->color[plane][row + 1][col - 1]), mask128);
+                //Load one pixels information into 3 128bit vectors
+                
                 
                 middle = _mm_mul_ps(middle, filt128.middle);
                 lower = _mm_fmadd_ps(lower, filt128.lower, middle);
                 upper = _mm_fmadd_ps(upper, filt128.upper, lower);
+                //Mul + fused mul + add to calculate values for the one pixel
+                
                 upper = _mm_hadd_ps(upper, upper);
                 upper = _mm_hadd_ps(upper, upper);
+                //Two horizontal adds leads the sum of the vector in each element
+                //The first sum is taken and divided, setting the output
                 output->color[plane][row][col] = fClamp255(upper[0]/divisor);
             }
         }
